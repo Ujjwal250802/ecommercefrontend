66ad8a09acdc4b328985f3cb0f240bbc28b17d4d
Ujjwal Kaushik <166251292+Ujjwal250802@users.noreply.github.com>
2025-07-19 21:02:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ecommercebackend-2-f367.onrender.com',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
